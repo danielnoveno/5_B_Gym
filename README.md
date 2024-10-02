55416811dfdf19a815cfd60bf33ff99f85e3b63b
danielnoveno <220711663@students.uajy.ac.id>
2024-10-02 15:38:41 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# tugas1github
+# gd_widget2_b_11663
 
 A new Flutter project.
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"tugas1github", origin, size)) {
+  if (!window.Create(L"gd_widget2_b_11663", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
