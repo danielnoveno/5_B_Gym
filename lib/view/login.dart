25ed16ac2d918b92038532c1e87140d6fe1e99f2
danielnoveno <220711663@students.uajy.ac.id>
2024-10-06 15:51:51 +0700

    update

---
+++ lib/view/login.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:gd_widget2_b_11663/view/home.dart';
+import 'package:gd_widget2_b_11663/view/register.dart';
+import 'package:gd_widget2_b_11663/component/form_component.dart';
+
+class LoginView extends StatefulWidget {
+  final Map? data; // meminta data ? artinya bisa null
+  const LoginView({super.key, this.data});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // fungsi untuk mendapatkan data dari inputan form field username dan password
+    TextEditingController usernameController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
+    // digunakan untuk memindahkan data, untuk passing
+    Map? dataForm = widget.data;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              inputForm((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return "Username tidak boleh kosong!";
+                }
+                return null;
+              },
+                  controller: usernameController,
+                  hintTxt: "Username",
+                  helperTxt: "Inputkan User yang telah didaftar",
+                  iconData: Icons.person),
+              inputForm((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return "Password tidak boleh kosong!";
+                }
+                return null;
+              },
+                  password: true,
+                  controller: passwordController,
+                  hintTxt: "Password",
+                  helperTxt: "Inputkan password",
+                  iconData: Icons.password),
+              Row( // inisnya button karena vertikal
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Cek apakah dataForm null
+                        if (dataForm != null &&
+                            dataForm['username'] == usernameController.text &&
+                            dataForm['password'] == passwordController.text) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const HomeView(),
+                            ),
+                          );
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (_) => AlertDialog(
+                              title: const Text('Password Salah'),
+                              content: TextButton(
+                                  onPressed: () => pushRegister(context),
+                                  child: const Text('Daftar disini!!')),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.pop(context, 'Cancel'),
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context, 'OK'),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      }
+                    },
+                    child: const Text('Login'),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Map<String, dynamic> formData = {};
+                        formData['username'] = usernameController.text;
+                        formData['password'] = passwordController.text;
+                        pushRegister(context);
+                      },
+                      child: const Text('Belum punya akun?')),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void pushRegister(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => const RegisterView()),
+    );
+  }
+}
