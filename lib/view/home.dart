ff27f3f17cf9040df818306ecadf3c00ffc417e4
danielnoveno <220711663@students.uajy.ac.id>
2024-10-06 15:54:28 +0700

    update home.dart

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gd_widget2_b_11663/view/view_list.dart';
 
+//home dart
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
