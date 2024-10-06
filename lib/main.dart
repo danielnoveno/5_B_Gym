220e512060a06d353b3e8041e95e777da52ff04d
Mariwow <220711664@students.uajy.ac.id>
2024-10-06 10:43:10 +0700

    ubah apa aja deh

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginView(),
+      home: LoginView("helo world"),
     );
   }
 }
