2a89eebd432d89666ff82353b28b85bbbf79c5e4
danielnoveno <220711663@students.uajy.ac.id>
2024-10-07 22:37:44 +0700

    upgrade register.dart

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -16,7 +16,7 @@ class _RegisterViewState extends State<RegisterView> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController notelpController = TextEditingController();
-  //tambah ini
+  //tambah ini tes
   TextEditingController tinggiController = TextEditingController();
   TextEditingController beratController = TextEditingController();
   TextEditingController umurController = TextEditingController();
