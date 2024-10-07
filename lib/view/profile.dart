98c493b2f7165c6db28048b808c30ed82921a796
StewartSidauruk <220711816@students.uajy.ac.id>
2024-10-07 20:08:27 +0700

    update for profile

--- lib/view/profile.dart
+++ lib/view/profile.dart
@@ -8,14 +8,21 @@ class profileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+<<<<<<< Updated upstream
         appBar: AppBar(
           title: Text('Profile'),
           leading: (
             IconButton(
+=======
+          appBar: AppBar(
+            title: Text('Profile'),
+            leading: (IconButton(
+>>>>>>> Stashed changes
               onPressed: () {
                 Navigator.pop(context);
               },
               icon: Icon(Icons.arrow_back),
+<<<<<<< Updated upstream
             )
           ),
         ),
@@ -88,3 +95,76 @@ class profileView extends StatelessWidget {
     );
   }
 }
+=======
+            )),
+          ),
+          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+          body: Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: Row(
+                    children: [
+                      kSizeBox2,
+                      CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('images/download.jpeg')),
+                      kSizeBox2,
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('User', style: kTextSytle4),
+                            Text('@user92u38128y', style: kTextStyle5),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        children: [
+                          Text('4', style: kTextStyle3),
+                          SizedBox(width: 4), // Add spacing between text
+                          Text('Posts', style: kTextStyle3),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('3', style: kTextStyle3),
+                          SizedBox(width: 4), // Add spacing between text
+                          Text('Followers', style: kTextStyle3),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('10', style: kTextStyle3),
+                          SizedBox(width: 4), // Add spacing between text
+                          Text('Following', style: kTextStyle3),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                kSizeBox,
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            label: Text('kembali'),
+            icon: const Icon(Icons.arrow_back),
+            backgroundColor: const Color.fromARGB(255, 85, 0, 159),
+          )),
+    );
+  }
+}
+>>>>>>> Stashed changes
