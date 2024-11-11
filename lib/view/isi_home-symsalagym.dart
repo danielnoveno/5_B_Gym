e1a5c21cf0583159193402226f7d9a7bd20deaf6
danielnoveno <220711663@students.uajy.ac.id>
2024-11-11 09:15:47 +0700

    generate time

---
+++ lib/view/isi_home-symsalagym.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppBarWIdget extends StatefulWidget {
+  @override
+  Widget build(BuildContext context){
+    return Padding(padding: padding)
+  }
+}
