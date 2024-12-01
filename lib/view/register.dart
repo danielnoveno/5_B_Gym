e4cd0784843fddffd0b3363fe22e379b7609ee48
iPenguwin <220711663@students.uajy.ac.id>
2024-12-01 10:03:14 +0700

    fix tampilan register

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -28,15 +28,16 @@ class _RegisterViewState extends State<RegisterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Kembali'),
+        title: const Text('Kembali', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.black,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
       ),
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Form(
@@ -110,22 +111,6 @@ class _RegisterViewState extends State<RegisterView> {
                 inputForm(
                   (p0) {
                     if (p0 == null || p0.isEmpty) {
-                      return 'Konfirmasi Password tidak boleh kosong';
-                    }
-                    if (p0 != passwordController.text) {
-                      return 'Password dan Konfirmasi Password tidak sama';
-                    }
-                    return null;
-                  },
-                  controller: confirmPasswordController,
-                  hintTxt: "Konfirmasi Password",
-                  helperTxt: "Konfirmasi Password Anda",
-                  iconData: Icons.password,
-                  password: true,
-                ),
-                inputForm(
-                  (p0) {
-                    if (p0 == null || p0.isEmpty) {
                       return 'Nomor Telepon tidak boleh kosong';
                     }
                     return null;
@@ -134,24 +119,6 @@ class _RegisterViewState extends State<RegisterView> {
                   hintTxt: "No Telp",
                   helperTxt: "Masukkan Nomor Telepon Anda",
                   iconData: Icons.phone_android,
-                ),
-                inputForm(
-                  (p0) {
-                    if (p0 == null || p0.isEmpty) {
-                      return 'Tanggal Lahir Tidak Boleh Kosong';
-                    }
-                    try {
-                      DateFormat format = DateFormat("dd/MM/yyyy");
-                      format.parse(p0); // Try parsing the entered date
-                    } catch (e) {
-                      return 'Format Tanggal Tidak Valid. Gunakan DD/MM/YYYY';
-                    }
-                    return null;
-                  },
-                  controller: tglLahirController,
-                  hintTxt: "DD/MM/YYYY",
-                  helperTxt: "Masukkan Tanggal Lahir Anda",
-                  iconData: Icons.date_range,
                 ),
                 inputForm(
                   (p0) {
@@ -168,11 +135,11 @@ class _RegisterViewState extends State<RegisterView> {
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 16.0),
                   child: SizedBox(
-                    width: 350,
+                    width: 390,
                     child: DropdownButtonHideUnderline(
                       child: DropdownButtonFormField<String>(
                         value: selectedValue,
-                        dropdownColor: Color(0xFF636363),
+                        dropdownColor: const Color(0xFF636363),
                         items: dropdownItems.map((item) {
                           return DropdownMenuItem(
                             value: item,
@@ -194,7 +161,7 @@ class _RegisterViewState extends State<RegisterView> {
                           helperText: 'Masukkan Peran Anda',
                           helperStyle: const TextStyle(color: Colors.white),
                           filled: true,
-                          fillColor: Color(0xFF636363),
+                          fillColor: const Color(0xFF636363),
                           prefixIcon: Icon(Icons.person, color: Colors.white),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20),
@@ -274,7 +241,7 @@ class _RegisterViewState extends State<RegisterView> {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(175, 194, 73, 255),
                     padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 135),
+                        vertical: 15, horizontal: 170),
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(14)),
                   ),
@@ -287,36 +254,37 @@ class _RegisterViewState extends State<RegisterView> {
     );
   }
 
-  // Define inputForm as a reusable form field widget
-  Widget inputForm(String? Function(String?) validator,
-      {required TextEditingController controller,
-      required String hintTxt,
-      required String helperTxt,
-      required IconData iconData,
-      bool password = false}) {
+  Padding inputForm(
+    String? Function(String?)? validator, {
+    required TextEditingController controller,
+    required String hintTxt,
+    required String helperTxt,
+    required IconData iconData,
+    bool password = false,
+  }) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+      padding: const EdgeInsets.all(10),
       child: TextFormField(
         controller: controller,
         obscureText: password,
+        validator: validator,
         style: const TextStyle(color: Colors.white),
         decoration: InputDecoration(
+          prefixIcon: Icon(iconData, color: Colors.white),
           filled: true,
           fillColor: const Color(0xFF636363),
           hintText: hintTxt,
           hintStyle: const TextStyle(color: Colors.white),
           helperText: helperTxt,
           helperStyle: const TextStyle(color: Colors.white),
-          prefixIcon: Icon(iconData, color: Colors.white),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide(color: Colors.white),
+            borderSide: const BorderSide(color: Colors.white),
           ),
         ),
-        validator: validator,
       ),
     );
   }
