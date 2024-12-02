eec01f84e2d8c907f03282e0dd02759b722f064a
danielnoveno <220711663@students.uajy.ac.id>
2024-12-02 22:47:18 +0700

    hilangkan konfirmasi password

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -29,7 +29,7 @@ class _RegisterViewState extends State<RegisterView> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('Kembali'), 
+        title: const Text('Kembali'),
         backgroundColor: Colors.black,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
@@ -105,22 +105,6 @@ class _RegisterViewState extends State<RegisterView> {
                   controller: passwordController,
                   hintTxt: "Password",
                   helperTxt: "Masukkan Password Yang Diinginkan",
-                  iconData: Icons.password,
-                  password: true,
-                ),
-                inputForm(
-                  (p0) {
-                    if (p0 == null || p0.isEmpty) {
-                      return 'Konfirmasi Password tidak boleh kosong';
-                    }
-                    if (p0 != passwordController.text) {
-                      return 'Password dan Konfirmasi Password tidak sama';
-                    }
-                    return null;
-                  },
-                  controller: confirmPasswordController,
-                  hintTxt: "Konfirmasi Password",
-                  helperTxt: "Konfirmasi Password Anda",
                   iconData: Icons.password,
                   password: true,
                 ),
