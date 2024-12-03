5ddc0ef8a53f07b276d862f19d69b327d172f4c3
AngelinaDerrel <220711674@students.uajy.ac.id>
2024-12-03 21:27:26 +0700

    add review dan segala macam isinya

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tubes_pbp_gym/providers/cart_provider.dart';
+import 'package:tubes_pbp_gym/view/home.dart';
 import 'package:tubes_pbp_gym/view/register.dart';
 import 'package:tubes_pbp_gym/view/login.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tubes_pbp_gym/view/riwayat.dart';
 
 void main() {
   runApp(
-    ProviderScope(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
       child: MyApp(),
     ),
   );
@@ -17,16 +23,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       onGenerateRoute: (settings) {
         switch (settings.name) {
-          case '/login':
-            return MaterialPageRoute(builder: (context) => const LoginView());
+          case '/home':
+            return MaterialPageRoute(builder: (context) => const HomeView());
           case '/register':
             return MaterialPageRoute(
                 builder: (context) => const RegisterView());
+          case '/home':
+            return MaterialPageRoute(builder: (context) => const HomeView());
           default:
-            return MaterialPageRoute(builder: (context) => const LoginView());
+            return MaterialPageRoute(builder: (context) => const Riwayat());
         }
       },
-      initialRoute: '/login',
+      initialRoute: '/home',
     );
   }
 }
